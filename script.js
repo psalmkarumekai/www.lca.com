683abac7c2bdd112effde1a1d686ca061dbe0e88
psalmkarumekai <psalm24shorty.t@gmail.com>
2024-05-20 13:48:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+ 
+//  let name = 'Psalm';
+//  console.log(name);
+ 
+
+//   let age = '3434';
+
+//   console.log('4' + '2');
+
+
+//   let isApproved = true;
+//   let isNotApproved = false;
+//   console.log((3 + 4) == 8);
+
+//   for (let i =0;i<5;i++){
+//     console.log(i)
+//   }
+//   let i = 0;
+//   while (i<5){
+//     console.log(i);
+//     i=i+1;
+//   }
+
+  let listExample = ['yaegun', 'Ethan', 'Psalm', 'Matthew']
+
+//   console.log(listExample[3])
+
+  for (let i = 0; i < listExample.length;i++){
+    console.log(listExample[i])
+  }
+
+
+
+
+  letfirstName = undefined;
